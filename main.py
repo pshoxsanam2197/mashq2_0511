a6e71af7fefcd674e109b2a858c15cd1b6fc67d5
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-05 18:13:19 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# 6
+for i in range(1, 51):
+    print(i)
+    if i % 7 == 0:
+        break
+
+# 7
+for i in range(1, 21):
+    print(i)
+    if i == 10:
+        break
+
+#8
+for i in range(1, 15, 2):
+    print(i)
+        break
+
+# 9
+for i in range(10, 1, -1):
+    print(i)
+    if i == 5:
+        break
+
+#10
+for i in range(1, 26):
+    if i % 4 == 0:
+        print(i)
